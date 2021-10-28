8eafc69c4130db8023eb2b3092880a413d19f488
Ufimskii <alexander1383@list.ru>
2021-10-28 22:04:13 +0500

    java 2.1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 6235;
+        int rubOneMiles = 20;
+        int totalMiles = ticketPrice / rubOneMiles;
+        System.out.println(totalMiles);
+    }
+}
